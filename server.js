1d46e9eb49f214ff7bebb43e8797126eb2a0c625
TimmocEdoc <fraggler131@gmail.com>
2020-10-21 14:56:29 +0700

    fix

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.static('./dist/cafe'));
+app.use(cors());
 
 app.get('/*', function (req, res) {
   res.sendFile('index.html', { root: 'dist/cafe' }
